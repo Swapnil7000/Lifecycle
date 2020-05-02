9075bfe5e97c12217099b49b179cc40b0b1e944a
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-05-02 21:11:18 +0530

    Initial commit

---
+++ app/src/main/java/com/swapnil/lifecycle/MainActivity.java
@@ -0,0 +1,47 @@
+package com.swapnil.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("MainActivity","onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v("MainActivity","onPause");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v("MainActivity","onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v("MainActivity","onDestroy");
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.v("MainActivity","onPostResume");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
